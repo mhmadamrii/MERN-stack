8bed573bd2c523b555661a4da4be5f47ab976577
mhmadamrii <interceptorghost4@gmail.com>
2022-12-21 19:56:38 +0700

    install env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const dotenv = require('dotenv');
+dotenv.config()
+const port = process.env.PORT || 5000;
 
 let todos = [
   {
